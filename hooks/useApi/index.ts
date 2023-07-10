58f5811f5f3bbc081dd4028c8835528a33ef6b92
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-07-10 19:55:37 -0300

    fix: adding .then to the fetchTrackingInfo request to work with the different interceptor logic

--- hooks/useApi/index.ts
+++ hooks/useApi/index.ts
@@ -5,7 +5,9 @@ import { IPackageProps } from "@/types";
 
 export const useApi = () => {
   const fetchTrackingInfo = async (code: string) => {
-    return api.get<IPackageProps>(`/prod?cod=${code}`);
+    return api
+      .get(`/prod?cod=${code}`)
+      .then(({ data }: { data: IPackageProps }) => data);
   };
 
   return { fetchTrackingInfo };
