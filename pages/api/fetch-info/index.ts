eb0df79908331ee1297e0ee72ac338f5d3c37c76
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-07-13 22:53:20 -0300

    feat: creating POST method to get code tracking and cities coordinates

---
+++ pages/api/fetch-info/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { fetchCoordinatesFromCities, fetchTrackingInfo } from "@/services/api";
+
+const validateErrors = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(400).json({ error: "Invalid Method!" });
+    return true;
+  }
+
+  const { code } = req.body;
+
+  if (!code) {
+    res.status(400).json({ error: "Invalid Code!" });
+    return true;
+  }
+
+  return false;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log = function () {};
+  if (validateErrors(req, res)) {
+    return res.end();
+  }
+
+  const { code } = req.body;
+
+  const filteredCities = await fetchTrackingInfo(code);
+
+  const citiesWithCoords = await fetchCoordinatesFromCities(filteredCities);
+
+  res.status(200).json(citiesWithCoords);
+};
+
+export default handler;
