9fca7460103fbf8ef7fc48bfda5562e67929b348
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-08-09 20:03:55 -0300

    feat: adding logic to show map only if there is a data

--- components/MainContainer/index.tsx
+++ components/MainContainer/index.tsx
@@ -3,7 +3,27 @@
 import React, { useEffect, useState } from "react";
 
 import { TrackingInfo } from "../TrackingInfo";
+import { ContainerSteps } from "./types";
+import { useTrackingInfo } from "@/context/TrackingInfoContext";
 
 export const MainContainer = () => {
-  return <TrackingInfo />;
+  const { info } = useTrackingInfo();
+  const [step, setStep] = useState(ContainerSteps.INITIAL);
+
+  const renderBasedOnStep = () => {
+    switch (step) {
+      case ContainerSteps.INITIAL:
+        return <></>;
+
+      case ContainerSteps.LOADED:
+        return <TrackingInfo />;
+    }
+  };
+
+  useEffect(() => {
+    if (!info.length) return setStep(ContainerSteps.INITIAL);
+    return setStep(ContainerSteps.LOADED);
+  }, [info]);
+
+  return renderBasedOnStep();
 };
